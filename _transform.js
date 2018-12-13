054d86240bdb429128aedeb5959cc2a04a266882
Samuel k. Mbaabu <samuel.karani@berkeley.edu>
2018-12-12 18:15:47 -0800

    updated transform into objects from arrays

--- _transform.js
+++ _transform.js
@@ -4,24 +4,24 @@ const essays = require("./essays.json")
 const data = essays.map(essay => transform(essay));
 
 function transform(essay) {
-  const newEssay = [
-    uuidv3('http://essaydatabase.org/essays', uuidv3.URL),
-    essay.paragraphs.join("\n\n"),
-    essay.prompt,
-    essay.college,
-    essay.yearApplied,
-    essay.applicationStatus,
-    essay.author,
-    "",
-    "United States",
-    essay.state,
-    essay.featured,
-    parseInt(essay.smallImageURL.split("=")[1], 10),
-    essay.dateUploaded,
-    "",
-    "",
-    0,
-  ]
+  const newEssay = {
+    id: uuidv3('http://essaydatabase.org/essays', uuidv3.URL),
+    essay: essay.paragraphs.join("\n\n"),
+    prompt: essay.prompt,
+    college: essay.college,
+    year: essay.yearApplied,
+    status: essay.applicationStatus,
+    author: essay.author,
+    email: "",
+    country: "United States",
+    state: essay.state,
+    featured: essay.featured.toString(),
+    image: parseInt(essay.smallImageURL.split("=")[1], 10),
+    date: essay.dateUploaded,
+    source: "",
+    comments: "",
+    views: 0,
+  }
   return newEssay;
 }
 
